d35cd5e76ab1150cf7156fd7f0afc41f7b6c77f5
wlLima <lima14.wl@gmail.com>
2023-07-18 21:56:34 -0300

    feat: added mobile menu and update responsivity

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,35 +1,54 @@
-import { FaCircleUser } from "react-icons/fa6";
+'use client'
+//import { FaCircleUser } from "react-icons/fa6";
+import { MdOutlineMenu } from "react-icons/md";
+import { MdAccountCircle } from "react-icons/md";
+import SideMenu from "../SideMenu";
 
 export default function Menu(){
     return(
-      <header className='flex justify-center drop-shadow bg-orange-500'>
-        <div className="flex p-2 justify-around gap-x-10 md:gap-x-32">
-          <div className="min-w-14">
-            <a href="/">
-              <img src="/logo-96.png" alt="Logo do projeto" className='w-14 h-14' />
-            </a>
-          </div>
-          <ul className="flex justify-end p-3 gap-3 text-white items-center">
-            <li>
-              <a href="/" className="hover:opacity-80">Início</a>
-            </li>
-            <li>
-              <a href="#" className="hover:opacity-80">Cardápio</a>
-            </li>
-            <li>
-              <a href="#" className="hover:opacity-80">Sobre</a>
-            </li>
-          </ul>
-          
-          <div className="flex items-center gap-x-2 text-white text-center min-w-32 hover:opacity-80">
-            <FaCircleUser className="text-xl" />
-            <a href="#" className="gap-y-0 min-w-[109px]">
-              Fazer Login
-              <p className="text-xs opacity-70">ou Registrar</p>
-            </a>
+      <>
+       <header className='flex justify-center drop-shadow bg-orange-400'>
+
+          <div className="flex p-2 justify-evenly gap-x-20 md:gap-x-32 w-full">
+
+            <div className="min-w-14">
+                <a href="/">
+                  <img src="/logo-96.png" alt="Logo do projeto" className='w-14 h-14' />
+                </a>
+            </div>
+
+            <ul className="md:flex md:justify-end md:p-3 md:gap-3 text-white md:items-center hidden">
+             <li>
+               <a href="/" className="hover:opacity-80">Início</a>
+             </li>
+             <li>
+               <a href="#" className="hover:opacity-80">Cardápio</a>
+             </li>
+             <li>
+               <a href="#" className="hover:opacity-80">Sobre</a>
+             </li>
+            </ul>
+
+            <div className="md:flex md:items-center md:gap-x-2 text-white text-center md:min-w-32 hover:opacity-80 hidden">
+             <MdAccountCircle className="text-xl" />
+             <a href="#" className="gap-y-0 min-w-[109px]">
+               Fazer Login
+               <p className="text-xs opacity-70">ou Registrar</p>
+             </a>
+            </div>
+
+            <div className="flex items-center cursor-pointer md:hidden">
+              <div className="text-white">
+                <MdOutlineMenu className="text-5xl" />
+              </div>
+            </div>
+            
+            <SideMenu />
+            
           </div>
 
-        </div>
-      </header>
+        </header>
+        
+      </>
     )
 }
