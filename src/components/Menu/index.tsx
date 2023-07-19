5975e89d5301cc51e6c7942da7448e417fe48f2e
wlLima <lima14.wl@gmail.com>
2023-07-19 00:11:26 -0300

    feat: add function to show side menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,31 @@
 'use client'
-//import { FaCircleUser } from "react-icons/fa6";
 import { MdOutlineMenu } from "react-icons/md";
+import { MdMenuOpen } from "react-icons/md";
 import { MdAccountCircle } from "react-icons/md";
-import SideMenu from "../SideMenu";
+import { useEffect, useState } from "react";
 
 export default function Menu(){
+
+  const [sideMenu, setSideMenu] = useState(false);
+
+  useEffect(()=>{
+    const wrapper = document.getElementById('wrapperSideMenu')
+
+    if(sideMenu === true){
+      wrapper?.classList.remove('hidden')
+    }else{
+      wrapper?.classList.add('hidden')
+    }
+
+  },[sideMenu])
+
+  function toggleSideMenu(){
+    setSideMenu(!sideMenu)
+  }
+
     return(
       <>
-       <header className='flex justify-center drop-shadow bg-orange-400'>
+       <header className='flex flex-col justify-center bg-orange-400 shadow-md'>
 
           <div className="flex p-2 justify-evenly gap-x-20 md:gap-x-32 w-full">
 
@@ -36,19 +54,46 @@ export default function Menu(){
                <p className="text-xs opacity-70">ou Registrar</p>
              </a>
             </div>
-
-            <div className="flex items-center cursor-pointer md:hidden">
+          
+            <a onClick={toggleSideMenu} className="flex items-center cursor-pointer md:hidden">
               <div className="text-white">
-                <MdOutlineMenu className="text-5xl" />
+                {
+                  sideMenu === true ? ( <MdMenuOpen className="text-5xl" /> ) : (<MdOutlineMenu className="text-5xl" />)   
+                }
               </div>
-            </div>
-            
-            <SideMenu />
+            </a>
             
           </div>
 
-        </header>
-        
+          <div id="wrapperSideMenu" className="absolute left-0 top-[4.5rem] bg-orange-400 p-10 w-1/2 h-side-menu hidden shadow-md md:hidden">
+
+            <div className="flex flex-col h-full">
+
+              <ul className="flex flex-col h-2/4 justify-start p-3 gap-8 text-white items-center">
+                <li>
+                  <a href="/" className="hover:opacity-80">Início</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:opacity-80">Cardápio</a>
+                </li>
+                <li>
+                  <a href="#" className="hover:opacity-80">Sobre</a>
+                </li>
+              </ul>
+       
+              <div className="flex items-end gap-x-2 text-white text-center min-w-32 hover:opacity-80 justify-center grow">
+                <MdAccountCircle className="text-xl" />
+                <a href="#" className="gap-y-0 min-w-[109px]">
+                  Fazer Login
+                  <p className="text-xs opacity-70">ou Registrar</p>
+                </a>
+              </div>
+
+            </div>
+
+          </div>
+
+        </header> 
       </>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      height: {
+        'side-menu': 'calc(100vh - 72px)',
+      }
     },
   },
   plugins: [],
