319dd9f70e8cabb88011ce07bc9fabc15381b2b0
wlLima <lima14.wl@gmail.com>
2023-07-28 17:03:00 -0300

    feat: added Carousel and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import Carousel from "@/components/Carousel";
 import Menu from "@/components/Menu";
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <div className="bg-slate-800">
+    <div>
       <Menu />
+      <Carousel />
+      <main className="flex flex-col items-center justify-between px-24 py-12 sm:px-8 h-full">
+        <div className="flex md:flex-col">
+          <img src="/hamburguers.jpg" alt="prato com hamburguers e batata" className="w-1/2 md:w-full" />
+          <div className="flex flex-col justify-center gap-5 bg-slate-50 text-center p-5 items-center">
+            <h1 className=" text-5xl font-bold">Sobre nós</h1>
+            <p className=" text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae debitis eum maiores architecto nesciunt porro molestiae, voluptatibus reiciendis facere natus, inventore aut necessitatibus! Velit nobis delectus, assumenda non quaerat deserunt.</p>
+            <button className=" bg-rose-600 p-3 text-white rounded-full w-56">Continue Lendo</button>
+          </div>
+        </div>
 
-      <main className="flex flex-col items-center justify-between p-24 h-full">
-        <Carousel />
         <div>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam vitae necessitatibus eaque, adipisci voluptate quaerat repellendus optio perspiciatis ducimus autem repellat deleniti excepturi dicta, corrupti sint delectus? Sit, rerum doloribus.
         </div>
@@ -66,6 +75,7 @@ export default function Home() {
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam vitae necessitatibus eaque, adipisci voluptate quaerat repellendus optio perspiciatis ducimus autem repellat deleniti excepturi dicta, corrupti sint delectus? Sit, rerum doloribus.
         </div>
       </main>
+      <Footer />
     </div>
   )
 }
--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -4,27 +4,22 @@ import { Carousel } from 'flowbite-react';
 
 export default function DefaultCarousel() {
   return (
-    <Carousel className='h-96 w-full'>
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
-      />
-      <img
-        alt="..."
-        src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
-      />
+    <Carousel slide={false} className='h-screen w-full'>
+      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+        <div className='h-full w-full'>
+          <img src="hamburguer1.jpg" alt="hamburguer" className='object-cover h-full w-full' />
+        </div>
+      </div>
+      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+        <div className='h-full w-full'>
+          <img src="hamburguer2.jpg" alt="hamburguer" className='object-cover h-full w-full' />
+        </div>
+      </div>
+      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+        <div className='h-full w-full'>
+          <img src="hamburguer3.jpg" alt="hamburguer" className='object-cover h-full w-full' />
+        </div>
+      </div>
     </Carousel>
   )
 }
